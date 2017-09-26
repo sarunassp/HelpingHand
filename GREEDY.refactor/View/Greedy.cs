d91772c962752c84414bd0451e736b9bfeabbc0e
sarunassp <sarunassp@gmail.com>
2017-09-26 20:14:17 +0300

    Adding windows forms UI

---
+++ GREEDY.refactor/View/Greedy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GREEDY.refactor.View
+{
+    public partial class Greedy : Form
+    {
+        public Greedy ()
+        {
+            InitializeComponent ();
+        }
+        
+        private void MainScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOCR_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textResult_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ItemList_CellContentClick()
+        {
+            
+        }
+    }
+}
