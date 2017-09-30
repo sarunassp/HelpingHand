8b30090da803dc922fec57e6d764d0256eff43c7
Valentinas <valentinas.kaminskas@mif.stud.vu.lt>
2017-10-01 02:43:43 +0300

    ShopDistributor method added

--- GREEDY/DataManagers/DataConverter.cs
+++ GREEDY/DataManagers/DataConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using GREEDY.Models;
+using System.Linq;
 
 namespace GREEDY.DataManagers
 {
@@ -11,9 +12,62 @@ namespace GREEDY.DataManagers
         /// </summary>
         /// <param name="receipt"></param>
         /// <returns></returns>
-        public List<Item> ReceiptToItemList (Receipt receipt)
+        /// 
+
+        private static ShopDistributor ShopDistributor => new ShopDistributor();
+
+        public List<Item> ReceiptToItemList(Receipt receipt)
         {
-            throw new System.NotImplementedException ();
+            var shop = ShopDistributor.ReceiptDistributor(receipt);
+            if (shop == "RIMI" || shop == "MAXIMA")
+            {
+                foreach (string line in receipt.LinesOfText)
+                {
+                    if (line.ToUpper().Contains("KVITAS") || line.ToUpper().Contains("PIRKĖJAS"))
+                    {
+                        throw new System.NotImplementedException();
+                    }
+                }
+
+                //    foreach (String line in receipt.LinesOfText)
+                //    {
+                //        _data += (line + Environment.NewLine);
+                //    }
+
+                //List<Item> itemsList = new List<Item>();
+                //foreach (var item in _data)
+                //{
+                //    itemsList.Add(new Item(item.Name, item.Price, item.Category));
+                //}
+
+                //    string pattern;
+                //    string input;
+
+                //    pattern = @"([*]+)\n(.+)\n([*]+)";
+                //    input = _data;
+                //    input = Regex.Replace(input, @"\r", "");
+
+                //    Match match = Regex.Match(input, pattern, RegexOptions.Singleline);
+                //    if (match.Success)
+                //    {
+                //        input = match.Groups[2].Value;
+                //        pattern = @"([^..]*)([.]+)( \d+,\d\d)";
+
+                //        MatchCollection matches = Regex.Matches(input, pattern, RegexOptions.Singleline);
+                //        foreach (Match m in matches)
+                //        {
+                //            itemlList.Add(new Item(m.Groups[1].Value, decimal.Parse(m.Groups[3].Value)));
+                //        }
+                //    }
+                //    return itemlList;
+                //}
+
+                return null;
+            }
+            else
+            {
+                throw new System.NotImplementedException();
+            }
         }
     }
 }
